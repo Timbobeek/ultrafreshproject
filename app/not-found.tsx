dfb7c1d3059c9142bc5dd2c8cce39859b5c2a0f8
Tim Goloshchapov <tim.goloschapov@gmail.com>
2025-07-31 16:24:26 -0500

    error page done

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,19 +1,41 @@
+"use client";
+
 import Image from "next/image";
 import arshavin from "../public/images/404page.png";
 import { Barlow_Condensed } from "next/font/google";
+import { useEffect } from "react";
+import { useHeader } from "../context/HeaderContext";
 
 const barlowCondensed = Barlow_Condensed({
-  weight: '500',
-  subsets: ['latin'],
-  display: 'swap',
-})
+  weight: "500",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function NotFound() {
-   return (
-     <div className={`${barlowCondensed.className} h-screen flex items-center justify-center flex-col text-center`}>
-      <p className="text-white text-medium absolute">PAGE NOT FOUND</p>
-      <Image src={arshavin} width={500} height={500} alt="404 message"/>
-     </div>
-   );
- }
- 
+  const [, setHeader] = useHeader();
+
+  useEffect(() => {
+    setHeader({
+      title: "Oooops",
+      subtext: null,
+      button: false,
+      redirect: "/",
+    });
+  }, []);
+  return (
+    <div
+      className={`${barlowCondensed.className} flex items-center justify-center flex-col text-center `}
+    >
+      <p className="text-white text-3xl sm:text-medium absolute">
+        PAGE NOT FOUND
+      </p>
+      <Image
+        src={arshavin}
+        width={600}
+        alt="404 message"
+        className="border-8 border-solid border-foreground rounded-3xl"
+      />
+    </div>
+  );
+}
