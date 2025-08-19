f0ac07b4e99a61c48c6c390667c4f2ac52446f72
Tim Goloshchapov <tim.goloschapov@gmail.com>
2025-08-19 16:01:20 -0500

    basic scrolling effect done

--- app/discussions/page.tsx
+++ app/discussions/page.tsx
@@ -44,13 +44,46 @@ export default function Page() {
       hover: "",
       link: "",
     },
+    {
+      title: "kickers",
+      text: "text-black",
+      image: "bg-[url('../public/surveyBoards/figo.jpeg')]",
+      borderColor: "border-orange-300",
+      backgroundColor: "bg-orange-300/75",
+      hover: "",
+      link: "",
+    },
+    {
+      title: "referees",
+      text: "text-black",
+      image: "bg-[url('../public/surveyBoards/torres.jpg')]",
+      borderColor: "border-rose-300",
+      backgroundColor: "bg-rose-300/75",
+      hover: "",
+      link: "",
+    },
+    {
+      title: "injuries",
+      text: "text-black",
+      image: "bg-[url('../public/surveyBoards/drogba.webp')]",
+      borderColor: "border-lime-300",
+      backgroundColor: "bg-lime-300/75",
+      hover: "",
+      link: "",
+    },
   ];
 
   return (
-    <div className="flex flex-col">
-      {homeBoardsContent.map((board, i) => (
-        <Board key={i} {...board} />
-      ))}
+    <div className="relative h-[550px] overflow-hidden flex flex-col ">
+      <div className="pointer-events-none absolute top-0 left-0 w-full h-8 bg-gradient-to-t from-transparent to-white" />
+      <main className="flex-1 overflow-y-auto scrollbar-none">
+        <div className="p-4 space-y-4">
+          {homeBoardsContent.map((board, i) => (
+            <Board key={i} {...board} />
+          ))}
+        </div>
+      </main>
+      <div className="pointer-events-none absolute bottom-0 left-0 w-full h-8 bg-gradient-to-t from-white to-transparent" />
     </div>
   );
 }
